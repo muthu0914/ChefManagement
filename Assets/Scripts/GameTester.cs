7a366ca742afc10be4207c85ce457a94e8d1567b
muthu0914 <muthu.rk0914@gmail.com>
2022-05-24 08:48:56 +0530

    Initialized Grid Module

---
+++ Assets/Scripts/GameTester.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameTester : MonoBehaviour
+{
+    public GridValues[] grid;
+    //private Grid grid;
+    private void Start()
+    {
+        foreach(GridValues g in grid)
+        {
+            GameObject newGrid = Instantiate(new GameObject());
+            newGrid.AddComponent<Grid>();
+            newGrid.GetComponent<Grid>().InitiateGrid(g);
+        }
+        
+    }
+
+    private void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    grid.SetValue(UtilsClass.GetMouseWorldPosition(), 77);
+        //}
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        //}
+    }
+
+    
+
+}
+
+[System.Serializable]
+public class GridValues
+{
+    public int height;
+    public int width;
+    public float cellSize;
+    public float offsetValue;
+    public Vector3 originPosition;
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid : MonoBehaviour
+{   
+    private int height;
+    private int width;
+    private float cellSize;
+    private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
+    private Vector3 gridOffset;
+    private Vector3 originPosition;
+
+    public Grid(int height, int width,float cellSize,float offsetValue,Vector3 originPosition)
+    {
+        this.height = height;
+        this.width = width;
+        this.cellSize = cellSize;
+        this.gridOffset = new Vector3(cellSize, cellSize) * offsetValue;
+        this.originPosition = originPosition;
+
+        gridArray = new int[height, width];
+        debugTextArray = new TextMesh[height, width];
+
+        for(int x=0;x< gridArray.GetLength(0); x++)
+        {
+            for(int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                debugTextArray[x,y]=CommonUtils.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + this.gridOffset, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1),Color.white,100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y),Color.white,100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    public void InitiateGrid(GridValues gridValues)
+    {
+        Grid grid = new Grid(gridValues.height, gridValues.width, gridValues.cellSize, gridValues.offsetValue, gridValues.originPosition);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x,y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y,int value)
+    {
+        if(x>=0 && y>=0 && x<width&& y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+}
