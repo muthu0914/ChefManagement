acfbd7aaa07b71ad35b76748f255f303c4367a5d
muthu0914 <muthu.rk0914@gmail.com>
2022-05-25 23:09:14 +0530

    Updated Grid as Generic Class

--- Assets/Scripts/GameTester.cs
+++ Assets/Scripts/GameTester.cs
@@ -5,17 +5,19 @@ using CodeMonkey.Utils;
 
 public class GameTester : MonoBehaviour
 {
-    public GridValues[] grid;
-    //private Grid grid;
+    //public GridValues[] grid;
+    private Grid<bool> grid;
     private void Start()
     {
-        foreach(GridValues g in grid)
-        {
-            GameObject newGrid = Instantiate(new GameObject());
-            newGrid.AddComponent<Grid>();
-            newGrid.GetComponent<Grid>().InitiateGrid(g);
-        }
-        
+        grid = new Grid<bool>(4, 2,5, 0.5f, new Vector3(-10, -5, 0));
+        new Grid<int>(4, 3,5, 0.5f, new Vector3(-40, -5, 0));
+        //foreach (GridValues g in grid)
+        //{
+        //    GameObject newGrid = Instantiate(new GameObject());
+        //    newGrid.AddComponent<Grid>();
+        //    newGrid.GetComponent<Grid>().InitiateGrid(g);
+        //}
+
     }
 
     private void Update()
@@ -34,12 +36,12 @@ public class GameTester : MonoBehaviour
 
 }
 
-[System.Serializable]
-public class GridValues
-{
-    public int height;
-    public int width;
-    public float cellSize;
-    public float offsetValue;
-    public Vector3 originPosition;
-}
+//[System.Serializable]
+//public class GridValues
+//{
+//    public int height;
+//    public int width;
+//    public float cellSize;
+//    public float offsetValue;
+//    public Vector3 originPosition;
+//}
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
-public class Grid : MonoBehaviour
+public class Grid<TGridObject>
 {   
     private int height;
     private int width;
     private float cellSize;
-    private int[,] gridArray;
+    private TGridObject[,] gridArray;
     private TextMesh[,] debugTextArray;
     private Vector3 gridOffset;
     private Vector3 originPosition;
@@ -21,14 +21,15 @@ public class Grid : MonoBehaviour
         this.gridOffset = new Vector3(cellSize, cellSize) * offsetValue;
         this.originPosition = originPosition;
 
-        gridArray = new int[height, width];
-        debugTextArray = new TextMesh[height, width];
+        gridArray = new TGridObject[width, height];
+        debugTextArray = new TextMesh[width, height];
 
+        GameObject grid = new GameObject();
         for(int x=0;x< gridArray.GetLength(0); x++)
         {
             for(int y = 0; y < gridArray.GetLength(1); y++)
             {
-                debugTextArray[x,y]=CommonUtils.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + this.gridOffset, 20, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x,y]=CommonUtils.CreateWorldText(gridArray[x, y].ToString(), grid.transform, GetWorldPosition(x, y) + this.gridOffset, 20, Color.white, TextAnchor.MiddleCenter);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1),Color.white,100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y),Color.white,100f);
             }
@@ -37,23 +38,25 @@ public class Grid : MonoBehaviour
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
-    public void InitiateGrid(GridValues gridValues)
-    {
-        Grid grid = new Grid(gridValues.height, gridValues.width, gridValues.cellSize, gridValues.offsetValue, gridValues.originPosition);
-    }
+    //public void InitiateGrid(GridValues gridValues)
+    //{
+    //    Grid grid = new Grid(gridValues.height, gridValues.width, gridValues.cellSize, gridValues.offsetValue, gridValues.originPosition);
+    //}
 
     private Vector3 GetWorldPosition(int x, int y)
     {
-        return new Vector3(x,y) * cellSize + originPosition;
+        //Debug.LogWarning(x + " " + y + " " + cellSize + " " + originPosition + " "+(new Vector3(x, y) * cellSize + originPosition));
+        return new Vector3(x, y) * cellSize + originPosition;
     }
 
     private void GetXY(Vector3 worldPosition, out int x, out int y)
     {
         x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
         y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+        //Debug.LogWarning(x + " " + y + " " + cellSize + " " + originPosition + " "+worldPosition);
     }
 
-    public void SetValue(int x, int y,int value)
+    public void SetValue(int x, int y, TGridObject value)
     {
         if(x>=0 && y>=0 && x<width&& y < height)
         {
@@ -62,14 +65,14 @@ public class Grid : MonoBehaviour
         }
     }
 
-    public void SetValue(Vector3 worldPosition, int value)
+    public void SetValue(Vector3 worldPosition, TGridObject value)
     {
         int x, y;
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
     }
 
-    public int GetValue(int x, int y)
+    public TGridObject GetValue(int x, int y)
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
@@ -77,11 +80,11 @@ public class Grid : MonoBehaviour
         }
         else
         {
-            return -1;
+            return default(TGridObject);
         }
     }
 
-    public int GetValue(Vector3 worldPosition)
+    public TGridObject GetValue(Vector3 worldPosition)
     {
         int x, y;
         GetXY(worldPosition, out x, out y);
