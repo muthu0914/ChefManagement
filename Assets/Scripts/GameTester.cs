158eb653b4d3f2b7324302514b10f6febf86f5ef
muthu0914 <muthu.rk0914@gmail.com>
2022-05-26 08:35:58 +0530

    Path Finding Module Initiated.

--- Assets/Scripts/GameTester.cs
+++ Assets/Scripts/GameTester.cs
@@ -1,47 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CodeMonkey.Utils;
+using Util;
 
 public class GameTester : MonoBehaviour
 {
-    //public GridValues[] grid;
-    private Grid<bool> grid;
+    [SerializeField] private PathfindingVisual pathfindingVisual;
+    private Pathfinding pathfinding;
     private void Start()
     {
-        grid = new Grid<bool>(4, 2,5, 0.5f, new Vector3(-10, -5, 0));
-        new Grid<int>(4, 3,5, 0.5f, new Vector3(-40, -5, 0));
-        //foreach (GridValues g in grid)
-        //{
-        //    GameObject newGrid = Instantiate(new GameObject());
-        //    newGrid.AddComponent<Grid>();
-        //    newGrid.GetComponent<Grid>().InitiateGrid(g);
-        //}
-
+        pathfinding = new Pathfinding(10, 10);
+        pathfindingVisual.SetGrid(pathfinding.GetGrid());
     }
 
     private void Update()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    grid.SetValue(UtilsClass.GetMouseWorldPosition(), 77);
-        //}
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
-        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = CommonUtils.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mousePosition, out int x, out int y);
+            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
+            if (path != null)
+            {
+                for(int i = 0; i < path.Count-1; i++)
+                {
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, 
+                        new Vector3(path[i + 1].x, path[i + 1].y) * 10f + Vector3.one * 5f, Color.red, 5f);
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePosition = CommonUtils.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mousePosition, out int x, out int y);
+            PathNode node = pathfinding.GetNode(x, y);
+            if (node != null)
+            {
+                node.SetIsWalkable(!node.isWalkable);
+            }
+        }
     }
 
-    
+
 
 }
-
-//[System.Serializable]
-//public class GridValues
-//{
-//    public int height;
-//    public int width;
-//    public float cellSize;
-//    public float offsetValue;
-//    public Vector3 originPosition;
-//}
---
+++ Assets/Scripts/Pathfinding/Grid.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+using System;
+
+public class Grid<TGridObject>
+{
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+    private const float offsetValue = 0.5f;
+    private int height;
+    private int width;
+    private float cellSize;
+    private TGridObject[,] gridArray;
+    private TextMesh[,] debugTextArray;
+    private Vector3 gridOffset;
+    private Vector3 originPosition;
+
+    public Grid(int width, int height,float cellSize,Vector3 originPosition, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject)
+    {
+        this.height = height;
+        this.width = width;
+        this.cellSize = cellSize;
+        this.gridOffset = new Vector3(cellSize, cellSize) * offsetValue;
+        this.originPosition = originPosition;
+
+        gridArray = new TGridObject[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        //GameObject grid = new GameObject();
+        for(int x=0;x< gridArray.GetLength(0); x++)
+        {
+            for(int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                gridArray[x,y] = createGridObject(this, x, y);
+                debugTextArray[x,y]=CommonUtils.CreateWorldText(gridArray[x, y].ToString(), 
+                    null, GetWorldPosition(x, y) + this.gridOffset, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1),Color.white,100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y),Color.white,100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) => {
+            debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y]?.ToString();
+        };
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void TriggerGridObjectChanged(int x, int y)
+    {
+        if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TGridObject value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetGridObject(x, y, value);
+    }
+
+    public TGridObject GetGridObject(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return default(TGridObject);
+        }
+    }
+
+    public TGridObject GetGridObject(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetGridObject(x, y);
+    }
+    //public void InitiateGrid(GridValues gridValues)
+    //{
+    //    Grid grid = new Grid(gridValues.height, gridValues.width, gridValues.cellSize, gridValues.offsetValue, gridValues.originPosition);
+    //}
+
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        //Debug.LogWarning(x + " " + y + " " + cellSize + " " + originPosition + " "+(new Vector3(x, y) * cellSize + originPosition));
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+        //Debug.LogWarning(x + " " + y + " " + cellSize + " " + originPosition + " "+worldPosition);
+    }
+}
